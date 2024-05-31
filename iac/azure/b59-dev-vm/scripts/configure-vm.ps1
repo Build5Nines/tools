0f223c9b38a71f5a3501920ac343af317073eb32
Chris Pietschmann <chris@build5nines.com>
2024-05-31 09:33:22 -0400

    Update configure-vm.ps1

--- iac/azure/b59-dev-vm/scripts/configure-vm.ps1
+++ iac/azure/b59-dev-vm/scripts/configure-vm.ps1
@@ -110,47 +110,47 @@ Set-ItemProperty -Path $HKLM -Name "DoNotOpenServerManagerAtLogon" -Value 1 -Typ
 # ############################################################
 function Install-Terraform {
      # Set the URL to check the latest Terraform version
-        $latestReleaseUrl = "https://api.github.com/repos/hashicorp/terraform/releases/latest"
+    $latestReleaseUrl = "https://api.github.com/repos/hashicorp/terraform/releases/latest"
 
-        # Define the installation directory
-        $installDir = "C:\Terraform"
+    # Define the installation directory
+    $installDir = "C:\Terraform"
 
-        # Create the installation directory if it does not exist
-        if (-Not (Test-Path -Path $installDir)) {
-            New-Item -ItemType Directory -Path $installDir
-        }
+    # Create the installation directory if it does not exist
+    if (-Not (Test-Path -Path $installDir)) {
+        New-Item -ItemType Directory -Path $installDir
+    }
 
-        # Check for the latest version
-        Write-Output "Checking for the latest Terraform version..."
-        $latestVersionResponse = Invoke-RestMethod -Uri $latestReleaseUrl
-        $latestVersion = $latestVersionResponse.tag_name.TrimStart("v")
+    # Check for the latest version
+    Write-Output "Checking for the latest Terraform version..."
+    $latestVersionResponse = Invoke-RestMethod -Uri $latestReleaseUrl
+    $latestVersion = $latestVersionResponse.tag_name.TrimStart("v")
 
-        Write-Output "Latest Terraform version is $latestVersion"
+    Write-Output "Latest Terraform version is $latestVersion"
 
-        # Construct the download URL for the Windows 64-bit binary
-        $downloadUrl = "https://releases.hashicorp.com/terraform/$latestVersion/terraform_${latestVersion}_windows_amd64.zip"
+    # Construct the download URL for the Windows 64-bit binary
+    $downloadUrl = "https://releases.hashicorp.com/terraform/$latestVersion/terraform_${latestVersion}_windows_amd64.zip"
 
-        # Define the paths for the downloaded zip file and the executable
-        $zipPath = "$installDir\terraform.zip"
-        $exePath = "$installDir\terraform.exe"
+    # Define the paths for the downloaded zip file and the executable
+    $zipPath = "$installDir\terraform.zip"
+    $exePath = "$installDir\terraform.exe"
 
-        # Download the latest Terraform zip file
-        Write-Output "Downloading Terraform $latestVersion..."
-        Download-File -url $downloadUrl -output $zipPath
+    # Download the latest Terraform zip file
+    Write-Output "Downloading Terraform $latestVersion..."
+    Download-File -url $downloadUrl -output $zipPath
 
-        # Extract the Terraform executable
-        Write-Output "Extracting Terraform executable..."
-        Add-Type -AssemblyName System.IO.Compression.FileSystem
-        [System.IO.Compression.ZipFile]::ExtractToDirectory($zipPath, $installDir)
+    # Extract the Terraform executable
+    Write-Output "Extracting Terraform executable..."
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipPath, $installDir)
 
-        # Cleanup the zip file
-        Remove-Item -Path $zipPath
+    # Cleanup the zip file
+    Remove-Item -Path $zipPath
 
-        # Add Terraform to the PATH
-        Add-Path $installDir
+    # Add Terraform to the PATH
+    Add-Path $installDir
 
-        Write-Output "Terraform $latestVersion installation completed."
-        # Write-Output "You may need to restart your terminal or system for the changes to take effect."
+    Write-Output "Terraform $latestVersion installation completed."
+    # Write-Output "You may need to restart your terminal or system for the changes to take effect."
 }
 
 function Install-Git {
